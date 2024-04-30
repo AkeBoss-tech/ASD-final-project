4ed586e9aa110731badd5e3b4eda17f9fb50e5a7
Akash Dubey <69588353+AkeBoss-tech@users.noreply.github.com>
2024-04-30 13:41:38 -0400

    beautiful camera

--- player.gd
+++ player.gd
@@ -1,15 +1,28 @@
 extends VehicleBody3D
 
-const MAX_STEER = 0.8
-const ENGINE_POWER = 300
+@export var max_steer = 0.6
+const ENGINE_POWER = 140000
 
+@onready var camera_pivot = $CameraPivot
+@onready var camera_3d = $CameraPivot/Camera3D
+
+var look_at
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURE)
-	pass # Replace with function body.
+	look_at = global_position
+	pass # Replace with function body.175
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	steering = move_toward(steering, Input.get_axis("ui_right", "ui_left") * MAX_STEER, delta * 2.5)
-	engine_force = Input.get_axis("ui_down", "ui_up") * ENGINE_POWER
+func _physics_process(delta):
+	steering = move_toward(steering, Input.get_axis("right", "left") * max_steer, delta * 2.5)
+	engine_force = Input.get_axis("down", "up") * ENGINE_POWER * delta
+	camera_pivot.global_position = camera_pivot.global_position.lerp(global_position, delta * 20.0)
+	camera_pivot.transform = camera_pivot.transform.interpolate_with(transform, delta * 5.0)
+	look_at = look_at.lerp(global_position + linear_velocity, delta * 5.0)
+	camera_3d.look_at(global_position + linear_velocity)
+	
+	if Input.is_action_pressed("ui_cancel"):
+		position = Vector3(position.x, position.y + 1, position.z)
+		rotation = Vector3(rotation.x, rotation.y, 0)
